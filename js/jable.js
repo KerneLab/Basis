018204bc91b4a753fe9a59324997f49c20679a00
核心力量 <kernelforce@gmail.com>
2013-10-24 23:41:15 +0800

    Refine some codes in jable.js If templateHead & templateBody had not been set, jable would generate each template according to the data structure received from server.
    Signed-off-by: 核心力量 <kernelforce@gmail.com>

--- js/jable.js
+++ js/jable.js
@@ -32,50 +32,82 @@
 			}
 		};
 
-		this.renderData = function(jsan, index) {
+		this.renderData = function(jsan, index, templateBody) {
 			if (index >= 0 && index < jsan.length) {
-				self.append(Tools.fillTemplate(opts.templateBody, jsan[index]));
+				if (templateBody == null) {
+					var columns = Tools.keys(jsan[index]);
+					var buffer = [ "<tr>" ];
+					for ( var i = 0; i < columns.length; i++) {
+						buffer.push("<td>");
+						buffer.push("?" + columns[i] + "?");
+						buffer.push("</td>");
+					}
+					buffer.push("</tr>");
+					templateBody = buffer.join("");
+				}
+				self.append(Tools.fillTemplate(templateBody, jsan[index]));
 				row = self.find("tr:last").hide().data(jsan[index]);
 				if ($.type(opts.onRenderRow) == "function") {
 					opts.onRenderRow(self.find("tr:last").hide(), index + 1);
 					opts.onShowingRow(row, index + 1, function() {
-						self.renderData(jsan, index + 1);
+						self.renderData(jsan, index + 1, templateBody);
 					});
-				}
-			} else {
-				if ($.type(opts.onRefreshed) == "function") {
-					opts.onRefreshed(jsan);
 				}
 			}
 		};
 
 		this.refresh = function() {
+			var url = opts.url;
+			var param = self.param();
+			var onRefreshed = opts.onRefreshed;
+			var templateHead = opts.templateHead;
+			var templateBody = opts.templateBody;
+
 			for ( var i in arguments) {
 				var arg = arguments[i];
 				switch ($.type(arg)) {
 				case "string":
-					opts.url = arg;
+					url = arg;
 					break;
 				case "object":
-					self.param(arg);
+					param = arg;
 					break;
 				case "function":
-					opts.onRefreshed = arg;
+					onRefreshed = arg;
 					break;
 				}
 			}
 
-			$.post(opts.url, self.param(), function(jsan) {
+			$.post(url, param, function(jsan) {
 				self.empty();
 
-				self.append(opts.templateHead);
+				if (templateHead == null && jsan.length > 0) {
+					var columns = Tools.keys(jsan[0]);
+					var buffer = [ "<tr>" ];
+					for ( var i = 0; i < columns.length; i++) {
+						buffer.push("<th>");
+						buffer.push(Tools.escapeText(columns[i]));
+						buffer.push("</th>");
+					}
+					buffer.push("</tr>");
+					templateHead = buffer.join("");
+				}
+
+				templateHead = templateHead == null ? "" : templateHead;
+
+				self.append(templateHead);
+
 				var row = self.find("tr:last").hide();
 				if ($.type(opts.onRenderRow) == "function") {
 					opts.onRenderRow(row, 0);
 				}
 				opts.onShowingRow(row, 0, function() {
-					self.renderData(jsan, 0);
+					self.renderData(jsan, 0, templateBody);
 				});
+
+				if ($.type(onRefreshed) == "function") {
+					onRefreshed(jsan);
+				}
 
 			}, "json");
 		};
@@ -84,8 +116,8 @@
 	};
 
 	$.fn.jable.defaults = {
-		"templateHead" : "",
-		"templateBody" : "",
+		"templateHead" : null,
+		"templateBody" : null,
 		"url" : "",
 		"param" : {},
 		"onRefreshed" : function(jsan) {
