a0d93e8c45448e5e0b0cf77c16da2d5351f52e7f
核心力量 <kernelforce@gmail.com>
2015-06-16 21:50:27 +0800

    segar now support minimum duration.
    Signed-off-by: 核心力量 <kernelforce@gmail.com>

--- js/segar.js
+++ js/segar.js
@@ -8,13 +8,11 @@
  * .segar-bar { background-color: #9999ee; color: #FFFFFF; float: left; height:
  * 20px; line-height: 20px; overflow: hidden; position: relative; text-align:
  * center; }
- * 
- * .segar-txt-hint { color: #333333; display: none; float: left; height: 20px;
- * left: 0px; line-height: 20px; position: absolute; text-align: center; top:
- * 0px; width: 200px; z-index: 100; }
  */
-(function($) {
-	$.fn.segar = function(options) {
+(function($)
+{
+	$.fn.segar = function(options)
+	{
 		var self = this;
 
 		var opts = $.extend({}, $.fn.segar.defaults, options);
@@ -26,10 +24,12 @@
 
 		this.trace = [];
 
-		this.sum = function() {
+		this.sum = function()
+		{
 			var data = Tools.get(arguments, 0, self.rates);
 			var sum = 0;
-			for ( var i in data) {
+			for ( var i in data)
+			{
 				sum += data[i]["value"];
 			}
 			return sum;
@@ -37,53 +37,60 @@
 
 		var rate = self.sum(self.rates);
 
-		this.traceRates = function(rates) {
+		this.traceRates = function(rates)
+		{
 			self.trace = [];
-			for ( var i in rates) {
+			for ( var i in rates)
+			{
 				self.trace.push(rates[i]);
 			}
 		};
 
 		box.empty();
 
-		if (!box.is("div")) {
+		if (!box.is("div"))
+		{
 			box = $("<div></div>");
 			this.append(box);
 		}
 
 		box.addClass(opts["box.class"]);
 
-		var darkText = $("<div></div>").addClass(opts["txt.hint.class"]).text(
-				opts["txt.formatter"](rate));
-		box.append(darkText);
-
 		this.bars = d3.select(self.get(0))
 				.selectAll("div." + opts["bar.class"]) //
-				.data(self.rates, function(d, i) {
+				.data(self.rates, function(d, i)
+				{
 					return d["id"] == null ? i : d["id"];
 				}).enter().append("div") //
 				.classed(opts["bar.class"], true) //
-				.style("background-color", function(d, i) {
+				.style("background-color", function(d, i)
+				{
 					return d["color"] == null ? null : d["color"];
 				})//
-				.attr("title", function(d, i) {
+				.attr("title", function(d, i)
+				{
 					var r = opts["txt.formatter"](d["value"]);
-					if (d["id"] != null) {
+					if (d["id"] != null)
+					{
 						r = d["id"] + "(" + r + ")";
 					}
 					return r;
 				}) //
-				.text(function(d, i) {
+				.text(function(d, i)
+				{
 					return opts["txt.formatter"](d["value"]);
 				}) //
-				.style("width", function(d, i) {
+				.style("width", function(d, i)
+				{
 					return (d["value"] * 100) + "%";
 				});
 
-		this.to = function(rates) {
-			if ($.type(rates) == "number") {
+		this.to = function(rates)
+		{
+			if ($.type(rates) == "number")
+			{
 				rates = [ {
-					"value" : rates
+					"value": rates
 				} ];
 			}
 
@@ -92,50 +99,51 @@
 			var rat = rate;
 			var node = self.get(0);
 			rate = self.sum(rates);
-			var duration = Math.abs(rate - rat) * opts["duration.total"];
+			var duration = Math.max(Math.abs(rate - rat)
+					* opts["duration.total"], opts["duration.least"]);
 
 			d3.select(self.get(0)).selectAll("div." + opts["bar.class"]) //
-			.data(rates, function(d, i) {
+			.data(rates, function(d, i)
+			{
 				return d["id"] == null ? i : d["id"];
 			}) //
-			.style("background-color", function(d, i) {
+			.style("background-color", function(d, i)
+			{
 				return d["color"] == null ? null : d["color"];
 			}) //
-			.attr("title", function(d, i) {
+			.attr("title", function(d, i)
+			{
 				var r = opts["txt.formatter"](d["value"]);
-				if (d["id"] != null) {
+				if (d["id"] != null)
+				{
 					r = d["id"] + "(" + r + ")";
 				}
 				return r;
 			}) //
 			.transition().duration(duration) //
-			.tween("text", function(d, i) {
+			.tween("text", function(d, i)
+			{
 				var a = self.trace[i];
 				var p = d3.interpolate(a == null ? 0 : a["value"], d["value"]);
 
-				return function(t) {
+				return function(t)
+				{
 					this.textContent = opts["txt.formatter"](p(t));
 				};
 			}) //
 			.styleTween(
 					"width",
-					function(d, i, a) {
+					function(d, i, a)
+					{
 						var p = d3.interpolate(parseFloat(a.substr(0,
 								a.length - 2))
 								/ self.width(), d["value"]);
 
-						return function(t) {
-							return (p(t) * 100).toFixed(2) + "%"
+						return function(t)
+						{
+							return (p(t) * 100).toFixed(2) + "%";
 						}
 					});
-
-			d3.select(node).selectAll("." + opts["txt.hint.class"]) //
-			.transition().duration(duration).tween("text", function(d, i, a) {
-				var p = d3.interpolate(rat, rate);
-				return function(t) {
-					this.textContent = opts["txt.formatter"](p(t));
-				};
-			});
 
 			self.data("segar.ratio", rates);
 
@@ -150,14 +158,15 @@
 	};
 
 	$.fn.segar.defaults = {
-		"init.rates" : [ {
-			"value" : 0
+		"init.rates": [ {
+			"value": 0
 		} ],
-		"duration.total" : 2000,
-		"box.class" : "segar-box",
-		"bar.class" : "segar-bar",
-		"txt.hint.class" : "segar-txt-hint",
-		"txt.formatter" : function(n) {
+		"duration.total": 2000,
+		"duration.least": 500,
+		"box.class": "segar-box",
+		"bar.class": "segar-bar",
+		"txt.formatter": function(n)
+		{
 			return (n * 100).toFixed(0) + "%";
 		}
 	};
