5cf87f76fff734d62025d697c7a40435591d8b6e
核心力量 <kernelforce@gmail.com>
2020-03-02 21:58:33 +0800

    Pair is available.

---
+++ src/org/kernelab/basis/Pair.java
@@ -0,0 +1,38 @@
+package org.kernelab.basis;
+
+import java.io.Serializable;
+
+public class Pair<K, V> implements Serializable
+{
+	/**
+	 * 
+	 */
+	private static final long	serialVersionUID	= -3040321140468343293L;
+
+	public final K				key;
+
+	public final V				value;
+
+	public Pair(K key, V value)
+	{
+		this.key = key;
+		this.value = value;
+	}
+
+	@Override
+	public boolean equals(Object o)
+	{
+		if (o instanceof Pair)
+		{
+			Pair<?, ?> p = (Pair<?, ?>) o;
+			return Tools.equals(this.key, p.key) && Tools.equals(this.value, p.value);
+		}
+		return false;
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return (key == null ? 0 : key.hashCode()) + (value == null ? 0 : value.hashCode());
+	}
+}
