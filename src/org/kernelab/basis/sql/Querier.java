280d21803b25e313e1f765df47555a2b30c17a62
核心力量 <kernelforce@gmail.com>
2023-08-16 22:24:38 +0800

    Querier.query method now throws SQLException.

--- src/org/kernelab/basis/sql/Querier.java
+++ src/org/kernelab/basis/sql/Querier.java
@@ -1,8 +1,9 @@
 package org.kernelab.basis.sql;
 
+import java.sql.SQLException;
 import java.util.Map;
 
 public interface Querier<T>
 {
-	public Iterable<T> query(SQLKit kit, Map<String, Object> params);
+	public Iterable<T> query(SQLKit kit, Map<String, Object> params) throws SQLException;
 }
